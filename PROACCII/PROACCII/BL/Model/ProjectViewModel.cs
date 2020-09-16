14ca2a0b4068d2f2ec62524f0d79b5d0a9698213
manojrmby <47946864+manojrmby@users.noreply.github.com>
2020-09-16 12:06:46 +0530

    Project Index

---
+++ PROACCII/PROACCII/BL/Model/ProjectViewModel.cs
@@ -0,0 +1,25 @@
+﻿using PROACCII_DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PROACCII.BL.Model
+{
+    public class ProjectViewModel
+    {
+        public System.Guid Project_Id { get; set; }
+        public string Project_Name { get; set; }
+        public string Description { get; set; }
+        public System.Guid Customer_Id { get; set; }
+        public System.Guid ProjectManager_Id { get; set; }
+        public int ScenarioId { get; set; }
+        public bool isActive { get; set; }
+        public System.DateTime Cre_on { get; set; }
+        public System.Guid Cre_By { get; set; }
+        public Nullable<System.DateTime> Modified_On { get; set; }
+        public Nullable<System.Guid> Modified_by { get; set; }
+        public bool IsDeleted { get; set; }       
+        public virtual List<Instance> Instances { get; set; }
+    }
+}
---
+++ PROACCII/PROACCII/Controllers/ProjectCreationController.cs
@@ -0,0 +1,35 @@
+﻿using PROACCII.BL.Model;
+using PROACCII_DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PROACCII.Controllers
+{
+    public class ProjectCreationController : Controller
+    {
+        private ProAccEntities db = new ProAccEntities(); 
+        // GET: ProjectCreation
+        public ActionResult Index()
+        {
+            List<ProjectViewModel> model = new List<ProjectViewModel>();
+            var projects = db.Projects.Where(x => x.isActive == true);
+            foreach (Project project in projects)
+            {
+                model.Add(new ProjectViewModel
+                {
+                    Project_Name= project.Project_Name,
+                    Cre_on=project.Cre_on,
+                    Customer_Id=project.Customer_Id,
+                    ProjectManager_Id=project.ProjectManager_Id,
+                    ScenarioId=project.ScenarioId,
+                    Instances=db.Instances.Where(x=>x.Project_ID==project.Project_Id).ToList()
+                });
+            }
+
+            return View(model);
+        }
+    }
+}
